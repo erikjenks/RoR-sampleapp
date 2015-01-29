e9527b5bcd34417158ca201466981e31a9d62805
Erik Jenks <erikjenks@gmail.com>
2015-01-28 22:34:36 -0500

    Finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,19 +4,24 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    asset_select "title", "Home | RoR Tutorial Sample App"
+    assert_select "title", "RoR Tutorial Sample App"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    asset_select "title" "Help | RoR Tutorial Sample App"
+    assert_select "title" "Help | RoR Tutorial Sample App"
   end
 
   test "should get about" do
   	get :about
   	assert_response :success
-  	asset_select "title" "About | RoR Tutorial Sample App"
+  	assert_select "title" "About | RoR Tutorial Sample App"
   end	
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | RoR Tutorial Sample App"
 
 end
